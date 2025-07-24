710e8b8537db58ce6a3a207fc86374ed1a3269a2
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-24 23:47:29 +0530

    add

--- Leetcode_10.java
+++ Leetcode_10.java
@@ -1,6 +1,12 @@
-public class Leetcode_10 {
-
-    public static void main(String args[]){
+class Leetcode_10 {
+    public double myPow(double x, int n) {
         
+        if(n>=0){
+         x =  Math.pow(x,n);
+        }
+        else{
+           x = Math.pow(x,n);
+        }
+        return x;
     }
 }
